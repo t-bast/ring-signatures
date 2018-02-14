d197b0db2aca9ad595549b7ecad20f38e950924d
t-bast <bastien@stratumn.com>
2018-02-14 13:43:30 +0100

    Bootstrap empty project files

---
+++ main.go
@@ -0,0 +1,10 @@
+// Package ring allows you to produce and verify ring signatures.
+package ring
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Work in progress...")
+}
---
+++ ring_test.go
@@ -0,0 +1,11 @@
+package ring_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWorkInProgress(t *testing.T) {
+	assert.True(t, true, "WIP")
+}
